5b3332d57e1c06093b4f51381a08f79d02cc3e28
Dhiraj Kumar <dhirajkumar.vcsmm@gmail.com>
2024-03-28 21:37:31 +0530

    example

---
+++ Switch.java
@@ -0,0 +1,16 @@
+public class switch {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String fruit = in.next();
+
+
+        if (fruit.equals("mango")){
+            system.out.println("king of fruit");
+
+        }
+        if (fruit.equal("apple")){
+            system.out.println("a sweet red fruit")
+        }
+    }
+    
+}
