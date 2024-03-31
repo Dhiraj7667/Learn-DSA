6789542e569f2ef7878c3abca2bd6a75af882e50
Dhiraj Kumar <dhirajkumar.vcsmm@gmail.com>
2024-03-31 22:55:05 +0530

    string

---
+++ Greeting.java
@@ -0,0 +1,8 @@
+public class Greeting {
+    public static void main(String[] args) {
+        Greeting();
+    }
+    static void Greeting(){
+        System.out.println("Hello World");
+    }
+}
---
+++ StringExample.java
@@ -0,0 +1,10 @@
+public class StringExample {
+    public static void main(String[] args) {
+        String message = greet();
+        System.out.println(message);
+    }
+    static String greet(){
+        String greeting = "how are you";
+        return greeting;
+    }
+}
