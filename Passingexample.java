345c83b7e0b5b5f5fc4a0285b42ad57c04699065
Dhiraj Kumar <dhirajkumar.vcsmm@gmail.com>
2024-04-01 23:17:47 +0530

    some example

---
+++ Passingexample.java
@@ -0,0 +1,13 @@
+public class Passingexample {
+    public static void main(String[] args) {
+        String name = "Dhiraj kumar";
+        greet(name);
+
+
+        }
+
+static void greet(String name){
+    System.out.println(name);
+}
+    }
+
